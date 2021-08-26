36fa38d6f85e027d7a0d9dfdc7c22b461272754b
Shandea <86807815+Shandea@users.noreply.github.com>
2021-08-26 11:04:47 -0700

    created current date component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/footer";
+import CurrentDate from "./components/CurrentDate";
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <CurrentDate />
       <Footer />
     </BrowserRouter>
   );
