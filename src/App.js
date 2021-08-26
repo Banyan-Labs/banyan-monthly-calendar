f2688c81aa23c2d8b8a566db0ddf96a7861af97e
Shandea <86807815+Shandea@users.noreply.github.com>
2021-08-26 12:59:32 -0700

    created training title component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/footer";
 import CurrentDate from "./components/CurrentDate";
-import "./App.css";
 import IntroTitle from "./components/IntroTitle";
+import TrainingTitle from "./components/TrainingTitle";
+import "./App.css";
+
 
 const App = () => {
   return (
     <BrowserRouter>
       <CurrentDate />
       <IntroTitle />
+      <TrainingTitle />
       <Footer />
     </BrowserRouter>
   );
