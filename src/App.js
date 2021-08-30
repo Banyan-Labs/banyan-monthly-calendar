ad9379529fd5c38551be779a9c305c6a9b20c57c
Pamela Augustine <pamela.augustine10@gmail.com>
2021-08-29 20:59:17 -0700

    created styled components; updated layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import IntroCard from './components/IntroCard/IntroCard';
 import Footer from "./components/footer";
-import CurrentDate from "./components/CurrentDate";
-import IntroTitle from "./components/IntroTitle";
-import TrainingTitle from "./components/TrainingTitle";
-import Notes from "./components/Notes";
 import "./App.css";
-
-
+import LandingPage from "./components/LandingPage";
 
 const App = () => {
   return (
 
-    <div className="App">    
+    <div>    
     <BrowserRouter>
-      <CurrentDate />
-      <IntroTitle />
-     <IntroCard />
-      <TrainingTitle />
-      <Notes />
+      <LandingPage />
       <Footer />
     </BrowserRouter>
   </div>
-
   );
 };
 
