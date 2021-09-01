73ae970cb0c46c73e1c33039c033b799eb63ade0
Pamela Augustine <pamela.augustine10@gmail.com>
2021-09-01 12:07:35 -0700

    media query profileCards

--- src/components/LandingPage/ProfileCards/style.js
+++ src/components/LandingPage/ProfileCards/style.js
@@ -5,4 +5,12 @@ export const Container = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
+    @media screen and (max-width: 770px) {
+    /* padding: 13px 0px 13px 0px; */
+    /* display: flex; */
+    flex-direction: column;
+    /* justify-content: space-evenly; */
+    /* align-items: right; */
+    }
 `;
+    
