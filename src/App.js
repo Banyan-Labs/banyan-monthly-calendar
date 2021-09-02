cecc7ae0e36042eea42b67da487423a427b8dbfa
wwells746 <70163623+wwells746@users.noreply.github.com>
2021-09-02 07:47:57 -0500

    Updated Navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import Footer from "./components/footer";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
 
     <div>    
     <BrowserRouter>
+      <NavBar />
       <LandingPage />
       <Footer />
     </BrowserRouter>
