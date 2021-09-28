de52d71cfc06ebd963ef5119c470dec301131a97
Shandea <86807815+Shandea@users.noreply.github.com>
2021-09-28 14:24:41 -0700

    updated fetch

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -5,31 +5,26 @@ import TrainingTitle from "./TrainingTitle";
 import ProfileCards from "./ProfileCards";
 import Notes from "./Notes";
 import TrainingCards from "./TrainingCards";
-
 import { Container, Wrapper } from "./style";
 
-// Get request goes here.
-// use axios or fetch to go to http://yourbackendURL.com/months
-// will return some object
-// from that object we only need the first item
+
 
 const LandingPage = () => {
 
   const [data, setData] = useState([])
   
   useEffect(() => {
-    // GET request using fetch inside useEffect React hook
     const backendUrl = 'type your heroku backend url here'
 
-    // change where we are fetching
     fetch('https://jsonplaceholder.typicode.com/posts')
+    // const backendUrl = 'https://banyan-cmc-backend.herokuapp.com/'
+    // fetch(backendUrl)
       .then((response) => response.json())
       .then((json) => {
         console.log(json)
         console.log(json[0].title)
         setData(json)
       });
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, []);
 
   return (
