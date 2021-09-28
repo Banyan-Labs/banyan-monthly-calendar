22fe81fc00afa54ac2229265d9f1d79d91713fb0
Shandea <86807815+Shandea@users.noreply.github.com>
2021-09-28 07:03:53 -0700

    updated admin form naming conventions

--- src/components/adminFrom/adminForm.jsx
+++ src/components/adminFrom/adminForm.jsx
@@ -9,7 +9,7 @@ import {
   AddPhotoButton,
   Select,
 } from "./style";
-import BillyJimBobWithAWig from "../../resource/images/avatar.png";
+import Photo from "../../resource/images/avatar.png";
 
 const AdminForm = () => {
   const [year, setYear] = useState("");
@@ -17,19 +17,19 @@ const AdminForm = () => {
   const [description, setDescription] = useState("");
 
   const options = [
-    { key: 1, text: "Select Month"},
-    { key: 2, text: "January"},
-    { key: 3, text: "Febuary"},
-    { key: 4, text: "March"},
-    { key: 5, text: "April"},
-    { key: 6, text: "May"},
-    { key: 7, text: "June"},
-    { key: 8, text: "July"},
-    { key: 9, text: "August"},
-    { key: 10, text: "September"},
-    { key: 11, text: "October"},
-    { key: 12, text: "November"},
-    { key: 13, text: "December"},
+    { key: 1, text: "Select Month" },
+    { key: 2, text: "January" },
+    { key: 3, text: "Febuary" },
+    { key: 4, text: "March" },
+    { key: 5, text: "April" },
+    { key: 6, text: "May" },
+    { key: 7, text: "June" },
+    { key: 8, text: "July" },
+    { key: 9, text: "August" },
+    { key: 10, text: "September" },
+    { key: 11, text: "October" },
+    { key: 12, text: "November" },
+    { key: 13, text: "December" },
   ];
 
   const onSubmit = (e) => {
@@ -48,7 +48,7 @@ const AdminForm = () => {
               </option>
             ))}
           </Select>
-      < br />
+          <br />
           <Input
             type="text"
             value={year}
@@ -57,7 +57,7 @@ const AdminForm = () => {
           />
         </Form>
         <Text>Introductions</Text>
-        <PersonPhoto src={BillyJimBobWithAWig} alt="Person Photo" />
+        <PersonPhoto src={Photo} alt="Person Photo" />
         {/* need upload photot option */}
         <AddPhotoButton placeholder="Add Photo" type="submit">
           Add Photo
