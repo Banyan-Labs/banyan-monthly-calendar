3a8c97e6281077d067abb8ae5e27c2376178d77e
Shandea <86807815+Shandea@users.noreply.github.com>
2021-10-04 14:33:51 -0700

    merged style into forms

--- src/components/adminForm/style.js
+++ src/components/adminForm/style.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 export const Container = styled.div`
   padding: 2rem 2rem;
   margin-bottom: 4%;
@@ -14,24 +13,23 @@ export const Text = styled.div`
     font-size: 10px;
   }
 `;
-
 export const Select = styled.select`
-margin: 0% 0% 1% 45%;
-width:10%;
+  margin: 0% 0% 1% 45%;
+  width:10%;
   padding: 3px;
 `;
 export const PersonPhoto = styled.div`
   width: 107px;
   height: 107px;
   border-radius: 50%;
-  background: #c4c4c4;
+  background: #C4C4C4;
   margin: 0% 0% 1% 46%;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 export const AddPhotoButton = styled.button`
-border:none;
+  border:none;
   margin-left: 45.5%;
   width: 10%;
   padding: 16px;
@@ -45,7 +43,7 @@ export const Form = styled.form`
   box-shadow: 0 5px 7px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
 `;
 export const Input = styled.input`
-margin: 0% 0% 1% 25%;
+  margin: 0% 0% 1% 25%;
   border:none;
   height:30px;
   width:50%;
@@ -53,9 +51,9 @@ margin: 0% 0% 1% 25%;
   box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19);
 `;
 export const SubmitButton = styled.button`
-border:none;
-margin: 0% 0% 1% 36%;
-width:30%;
+   border:none;
+   margin: 0% 0% 1% 36%;
+   width:30%;
    padding: 16px;
    box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24),
    0 17px 50px 0 rgba(0, 0, 0, 0.19);
